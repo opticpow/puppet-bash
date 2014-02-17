84ba7b56341bb3c81238a91847e395d6299f48b4
Wayne Ingram <w.ingram@nib.com.au>
2014-02-17 14:00:36 +1100

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class to manage bash
+class bash {
+
+  package {
+    'bash':
+      ensure => present
+  }
+
+  file {
+    '/etc/skel/.bashrc':
+      ensure  => present,
+      owner   => root,
+      group   => root,
+      mode    => 0444,
+      source  => 'puppet:///modules/bash/bashrc',
+      require => Package['bash']
+  }
